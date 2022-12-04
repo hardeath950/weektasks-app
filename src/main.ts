c41ad2a6a0bb9f39e47dd8cdcf39b938ba0c07ce
Mateus Sarmento <omateusmento@gmail.com>
2022-12-04 12:15:32 -0300

    Format main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import "./assets/main.css";
+
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
-
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(router);
+app.use(createPinia());
 
 app.mount("#app");
