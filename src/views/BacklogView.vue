30db09f08be1c4f112697cf00912a467bc4935c8
Mateus Sarmento <omateusmento@gmail.com>
2022-12-05 19:41:48 -0300

    Add remove issues feature

--- src/views/BacklogView.vue
+++ src/views/BacklogView.vue
@@ -3,7 +3,7 @@
     <ul>
       <li v-for="issue in issues" :key="issue.id">
         {{ issue.title }}
-        <button>
+        <button @click="removeIssue(issue.id)">
           <el-icon><Delete /></el-icon>
         </button>
       </li>
@@ -14,7 +14,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import axios from "axios";
-import { Delete, Plus } from "@element-plus/icons-vue";
+import { Delete } from "@element-plus/icons-vue";
 
 const issues = ref<any[]>([]);
 
@@ -22,6 +22,11 @@ onMounted(async () => {
   let { data } = await axios.get<any[]>("http://localhost:3000/issues");
   issues.value = data;
 });
+
+async function removeIssue(issueId: number) {
+  await axios.delete<any[]>("http://localhost:3000/issues/" + issueId);
+  issues.value = issues.value.filter((i) => i.id !== issueId);
+}
 </script>
 
 <style scoped></style>
