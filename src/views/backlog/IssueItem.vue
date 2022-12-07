cb2fa9b38be1fa28f79e5a6103d5b05b5bf9dac0
Mateus Sarmento <omateusmento@gmail.com>
2022-12-07 07:54:46 -0300

    Add IssueItem

---
+++ src/views/backlog/IssueItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="issue">
+    {{ issue.title }}
+    <button @click="removeIssue(issue.id)">
+      <el-icon><Delete /></el-icon>
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  issue: { id: number; title: string };
+}>();
+
+let emit = defineEmits(["remove"]);
+
+function removeIssue(id: number) {
+  emit("remove", { issueType: "issue", id });
+}
+</script>
