e74c837d92385710c944b7818867b504f014c924
Mateus Sarmento <omateusmento@gmail.com>
2022-12-07 11:54:03 -0300

    Add layout for creating issues in sprint

--- src/views/backlog/SprintItem.vue
+++ src/views/backlog/SprintItem.vue
@@ -1,11 +1,20 @@
 <template>
-  {{ sprint.title }}
-  <button @click="removeSprint(sprint.id)">
-    <el-icon><Delete /></el-icon>
-  </button>
+  <div>
+    {{ sprint.title }}
+    <button @click="removeSprint(sprint.id)">
+      <el-icon><Delete /></el-icon>
+    </button>
+  </div>
+  <form @submit.prevent="createSprint">
+    <input v-model="sprintTitle" />
+    <button type="submit">
+      <el-icon><Plus /></el-icon>
+    </button>
+  </form>
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
 import type { Sprint } from "./sprint.model";
 
 defineProps<{
@@ -13,6 +22,10 @@ defineProps<{
 }>();
 
 let emit = defineEmits(["remove"]);
+
+let sprintTitle = ref("");
+
+function createSprint() {}
 
 function removeSprint(id: number) {
   emit("remove", id);
