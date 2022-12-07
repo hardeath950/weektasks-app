0ecb43437a3f8d07848dd02a43bbcc9724027fca
Mateus Sarmento <omateusmento@gmail.com>
2022-12-07 08:17:10 -0300

    Add create issue in epic feature

--- src/views/backlog/EpicItem.vue
+++ src/views/backlog/EpicItem.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="epic">
-    {{ issue.title }}
-    <button @click="removeEpic(issue.id)">
-      <el-icon><Delete /></el-icon>
-    </button>
+    <div>
+      {{ issue.title }}
+      <button @click="removeEpic(issue.id)">
+        <el-icon><Delete /></el-icon>
+      </button>
+    </div>
+    <form @submit.prevent="createIssue">
+      <input v-model="issueTitle" />
+      <button>
+        <el-icon><Plus /></el-icon>
+      </button>
+    </form>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
+import axios from "axios";
 import type { Issue } from "./issue.model";
 
-defineProps<{
+let props = defineProps<{
   issue: Issue;
 }>();
 
 let emit = defineEmits(["remove"]);
+
+const issueTitle = ref("");
+
+async function createIssue() {
+  const issue = { title: issueTitle.value, epic: props.issue };
+  await axios.post<any[]>("http://localhost:3000/issues", issue);
+  issueTitle.value = "";
+}
 
 function removeEpic(id: number) {
   emit("remove", "epic", id);
