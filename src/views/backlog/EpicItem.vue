e9458f0311ee93f5e7e36b7dfc6b316a7c9615d7
Mateus Sarmento <omateusmento@gmail.com>
2022-12-07 07:54:56 -0300

    Add EpicItem

---
+++ src/views/backlog/EpicItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="epic">
+    {{ issue.title }}
+    <button @click="removeEpic(issue.id)">
+      <el-icon><Delete /></el-icon>
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  issue: { id: number; title: string };
+}>();
+
+let emit = defineEmits(["remove"]);
+
+function removeEpic(id: number) {
+  emit("remove", { issueType: "epic", id });
+}
+</script>
