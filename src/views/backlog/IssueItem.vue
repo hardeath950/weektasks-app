cd99f14d403c8952cd75ed07bccc0598e86931e8
Mateus Sarmento <omateusmento@gmail.com>
2022-12-08 02:44:07 -0300

    Send updated issue title to the api

--- src/views/backlog/IssueItem.vue
+++ src/views/backlog/IssueItem.vue
@@ -3,6 +3,9 @@
     <WkEditable v-model="issueTitle" :editable="editable" />
 
     <div class="actions">
+      <button v-if="editable" @click="updateIssueTitle">
+        <el-icon><Check /></el-icon>
+      </button>
       <button @click="editable = !editable">
         <el-icon><Edit /></el-icon>
       </button>
@@ -15,7 +18,8 @@
 
 <script lang="ts" setup>
 import WkEditable from "@/components/form/WkEditable.vue";
-import { Edit } from "@element-plus/icons-vue";
+import { Check, Edit } from "@element-plus/icons-vue";
+import axios from "axios";
 import { ref, computed } from "vue";
 
 let props = defineProps<{
@@ -28,11 +32,16 @@ let editable = ref(false);
 
 let issueTitle = computed({
   get: () => props.issue.title,
-  set: (v) => emit("update:issue", v),
+  set: (title) => emit("update:issue", { ...props.issue, title }),
 });
 
 function removeIssue(id: number) {
   emit("remove", id);
+}
+
+async function updateIssueTitle() {
+  let patch = { title: issueTitle.value };
+  await axios.patch("http://localhost:3000/issues/" + props.issue.id, patch);
 }
 </script>
 
