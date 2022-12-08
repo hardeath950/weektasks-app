88406d0ab9a58f7205e968e02c9ee4d96e13a457
Mateus Sarmento <omateusmento@gmail.com>
2022-12-08 01:58:00 -0300

    Use WkEditable and move action buttons to the right

--- src/views/backlog/IssueItem.vue
+++ src/views/backlog/IssueItem.vue
@@ -1,20 +1,47 @@
 <template>
   <div class="issue">
-    {{ issue.title }}
-    <button @click="removeIssue(issue.id)">
-      <el-icon><Delete /></el-icon>
-    </button>
+    <WkEditable v-model="issueTitle" :editable="editable" />
+
+    <div class="actions">
+      <button @click="editable = !editable">
+        <el-icon><Edit /></el-icon>
+      </button>
+      <button @click="removeIssue(issue.id)">
+        <el-icon><Delete /></el-icon>
+      </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-defineProps<{
+import WkEditable from "@/components/form/WkEditable.vue";
+import { Edit } from "@element-plus/icons-vue";
+import { ref, computed } from "vue";
+
+let props = defineProps<{
   issue: { id: number; title: string };
 }>();
 
-let emit = defineEmits(["remove"]);
+let emit = defineEmits(["remove", "update:issue"]);
+
+let editable = ref(false);
+
+let issueTitle = computed({
+  get: () => props.issue.title,
+  set: (v) => emit("update:issue", v),
+});
 
 function removeIssue(id: number) {
   emit("remove", id);
 }
 </script>
+
+<style scoped>
+.issue {
+  display: flex;
+}
+
+.actions {
+  margin-left: auto;
+}
+</style>
