e548a334364f3ce949aeeb8074102607d349b413
Mateus Sarmento <omateusmento@gmail.com>
2023-02-07 15:30:31 -0300

    Add empty placeholder for epic issues

--- src/views/backlog/EpicItem.vue
+++ src/views/backlog/EpicItem.vue
@@ -14,24 +14,9 @@
         </button>
       </div>
     </div>
-    <div class="issues">
-      <draggable v-model="epicIssues" item-key="id" tag="ul" group="issues" @change="moveIssue">
-        <template #item="{ element: issue, index: i }">
-          <li>
-            <IssueItem
-              v-model:issue="epicIssues[i]"
-              @remove="removeIssue(issue.id)"
-            />
-          </li>
-        </template>
-      </draggable>
-      <form @submit.prevent="createIssue" class="create-issue">
-        <input v-model="issueTitle" placeholder="Insira uma nova questão"/>
-        <button>
-          <el-icon><Plus /></el-icon>
-        </button>
-      </form>
-    </div>
+
+    <EpicIssues :epic="epic" @update:epic="emit('update:epic', $event)" @update:issues="emit('update:issues', $event)"/>
+
   </div>
 </template>
 
@@ -40,8 +25,7 @@ import { ref, computed } from "vue";
 import { axios } from "./axios";
 import type { Epic } from "./issue.model";
 import WkEditable from "@/components/form/WkEditable.vue";
-import IssueItem from "./IssueItem.vue";
-import draggable from "vuedraggable";
+import EpicIssues from "./epic/EpicIssues.vue";
 
 let props = defineProps<{
   epic: Epic;
@@ -55,12 +39,6 @@ let epicTitle = computed({
   set: (title: any) => emit("update:epic", patchEpic({ title })),
 });
 
-let epicIssues = computed({
-  get: () => props.epic.issues,
-  set: (issues: any) => emit("update:issues", issues),
-});
-
-const issueTitle = ref("");
 const editable = ref(false);
 
 function patchEpic(patch: Partial<Epic>) {
@@ -76,62 +54,11 @@ async function updateEpicTitle() {
 function removeEpic(id: number) {
   emit("remove", id);
 }
-
-async function createIssue() {
-  const issue = { title: issueTitle.value };
-  let url = `/epics/${props.epic.id}/issues`;
-  let { data } = await axios.post(url, issue);
-  epicIssues.value = [...props.epic.issues, data];
-  issueTitle.value = "";
-}
-
-async function removeIssue(id: number) {
-  await axios.delete("/issues/" + id);
-  epicIssues.value = props.epic.issues.filter((i) => i.id !== id);
-}
-
-async function moveIssue({ moved, added, removed }: any) {
-  if (added) {
-    let { id } = added.element;
-    let url = `/backlog/epics/${props.epic.id}/issues/${id}`;
-    axios.post(url, { order: added.newIndex });
-  }
-
-  if (removed) {
-    let { id } = removed.element;
-    let url = `/backlog/epics/${props.epic.id}/issues/${id}/soft-remove`;
-    axios.delete(url);
-  }
-
-  if (moved) {
-    let { id } = moved.element;
-    let url = `/backlog/epics/${props.epic.id}/issues/${id}/order`
-    axios.post(url, { order: moved.newIndex });
-  }
-}
 </script>
 
 <style lang="scss" scoped>
 .epic-topbar {
   display: flex;
-}
-
-.issues {
-  margin-top: 5px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-.issue {
-  border-bottom: 1px solid #ccc;
-  padding: 5px;
-}
-
-.create-issue {
-  padding: 5px;
-  display: flex;
-  input {
-    width: 100%;
-  }
 }
 
 .actions {
---
+++ src/views/backlog/epic/EpicIssues.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="issues">
+      <draggable v-model="epicIssues" item-key="id" tag="ul" group="issues" @change="moveIssue">
+        <template #item="{ element: issue, index: i }">
+          <li>
+            <IssueItem
+              v-model:issue="epicIssues[i]"
+              @remove="removeIssue(issue.id)"
+            />
+          </li>
+        </template>
+        <template #footer v-if="epicHasNoIssue">
+          <li class="issue">
+            <small>
+              <i>Mova uma questão aqui</i>
+            </small>
+          </li>
+        </template>
+      </draggable>
+      <form @submit.prevent="createIssue" class="create-issue">
+        <input v-model="issueTitle" placeholder="Insira uma nova questão"/>
+        <button>
+          <el-icon><Plus /></el-icon>
+        </button>
+      </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { axios } from "../axios";
+import type { Epic } from "../issue.model";
+import IssueItem from "../IssueItem.vue";
+import draggable from "vuedraggable";
+
+let props = defineProps<{
+  epic: Epic;
+}>();
+
+let emit = defineEmits(["remove", "update:epic", "update:issues"]);
+
+let epicHasNoIssue = computed(() => props.epic.issues.length === 0);
+
+let epicIssues = computed({
+  get: () => props.epic.issues,
+  set: (issues: any) => emit("update:issues", issues),
+});
+
+const issueTitle = ref("");
+
+async function createIssue() {
+  const issue = { title: issueTitle.value };
+  let url = `/epics/${props.epic.id}/issues`;
+  let { data } = await axios.post(url, issue);
+  epicIssues.value = [...props.epic.issues, data];
+  issueTitle.value = "";
+}
+
+async function removeIssue(id: number) {
+  await axios.delete("/issues/" + id);
+  epicIssues.value = props.epic.issues.filter((i) => i.id !== id);
+}
+
+async function moveIssue({ moved, added, removed }: any) {
+  if (added) {
+    let { id } = added.element;
+    let url = `/backlog/epics/${props.epic.id}/issues/${id}`;
+    axios.post(url, { order: added.newIndex });
+  }
+
+  if (removed) {
+    let { id } = removed.element;
+    let url = `/backlog/epics/${props.epic.id}/issues/${id}/soft-remove`;
+    axios.delete(url);
+  }
+
+  if (moved) {
+    let { id } = moved.element;
+    let url = `/backlog/epics/${props.epic.id}/issues/${id}/order`
+    axios.post(url, { order: moved.newIndex });
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.issues {
+  margin-top: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+.issue {
+  border-bottom: 1px solid #ccc;
+  padding: 5px;
+}
+
+.create-issue {
+  padding: 5px;
+  display: flex;
+  input {
+    width: 100%;
+  }
+}
+</style>
